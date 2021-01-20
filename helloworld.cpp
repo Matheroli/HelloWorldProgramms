d0d085b9d8d3b94959b1beda5d854e87d0658463
Matheroli <68776425+Matheroli@users.noreply.github.com>
2021-01-20 10:30:33 +0100

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,4 +3,10 @@
 using namespace std;
 
 int main() {
+  string a = "Hello World";
+  if (a=="Hello World") {
+    cout << "a est bien égal à Hello World";
+  } else{
+    cout << "a n'est pas égal à Hello World";
+  }
 }
