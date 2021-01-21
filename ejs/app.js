c97d9cc3c8868446833cbb91344ffa31e5ea5ca6
Matheroli <68776425+Matheroli@users.noreply.github.com>
2021-01-21 11:21:09 +0100

    Update app.js

--- ejs/app.js
+++ ejs/app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express(); 
 
 app.get ("/", function (req,res) {
-	res.render ( "main.ejs" );	
+	res.render ( "helloworld.ejs" );	
 	} );
 app.listen(3000 , function () {
 	console.log ("Le serveur est prêt");
