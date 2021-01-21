0fb6e998c0b7d35e2fd39894cc354ceecafa8abb
Matheroli <68776425+Matheroli@users.noreply.github.com>
2021-01-21 11:20:55 +0100

    Create app.js

---
+++ ejs/app.js
@@ -0,0 +1,9 @@
+const express = require ('express'); 
+const app = express(); 
+
+app.get ("/", function (req,res) {
+	res.render ( "main.ejs" );	
+	} );
+app.listen(3000 , function () {
+	console.log ("Le serveur est prêt");
+} );
