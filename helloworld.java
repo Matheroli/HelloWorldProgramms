a51f205d2db0c5f349eb25ee0721a20c3ccf29e0
Matheroli <68776425+Matheroli@users.noreply.github.com>
2021-01-21 10:49:47 +0100

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,1 +1,12 @@
+public class HelloWorld {
 
+    public static void main(String[] args) {
+      string a = "Hello World";
+      if (a=="Hello World") {
+        System.out.println("a est bien égal à Hello World");
+      } else{
+        System.out.println("Ok");
+      }
+    }
+
+}
