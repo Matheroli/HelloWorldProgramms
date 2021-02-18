c77b011c28429bb6e529049a6985970e639e085e
Matheroli <68776425+Matheroli@users.noreply.github.com>
2021-02-18 17:39:31 +0100

    Update app.js

--- ejs/app.js
+++ ejs/app.js
@@ -1,9 +1,4 @@
-const express = require ('express'); 
-const app = express(); 
-
-app.get ("/", function (req,res) {
-	res.render ( "helloworld.ejs" );	
-	} );
-app.listen(3000 , function () {
-	console.log ("Le serveur est prêt");
-} );
+const ejs = require('ejs');
+let a = ['Hello World'];
+let html = ejs.render('<%= a %>', {a: a});
+//Je suis sûr que c'est faux :/
